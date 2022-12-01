d227b99679c3c923970772274c099e2111bb60bb
[Simon] <[simon.sandevik@outlook.com]>
2022-12-01 20:52:47 +0100

    working animation'

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "../styles/Hero.module.css"
+
+export default function Hero() {
+  return (
+    <div className={style.container}>
+        <h1 className={style.h1}>Omstartslån</h1>
+    </div>
+  )
+}
