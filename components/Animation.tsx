12a8315286a37bc9707af71ba4848f5752bf379b
[Simon] <[simon.sandevik@outlook.com]>
2022-12-01 09:38:37 +0100

    useAnimation hook created

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useAnimation } from "../hooks/useAnimation";
 import style from "../styles/AnimationComponent.module.css";
 
 type AnimationType = "slideFromLeft" | "slideFromRight" | "fadeIn";
@@ -10,35 +11,8 @@ interface AnimationProps {
 }
 
 export default function Animation({ children, animationType, boxHeight }: AnimationProps) {
-  const [isInViewPort, setIsInViewPort] = useState<boolean>(false);
-  const [randId, setRandId] = useState<number>(0);
-
-  function isElementInViewport(el: Element | null) {
-    let rect = el
-      ? el.getBoundingClientRect()
-      : { bottom: 0, left: 0, right: 0, top: 0 };
-
-    return (
-      rect.bottom > 0 &&
-      rect.top < (window.innerHeight || document.documentElement.clientHeight)
-    );
-  }
-
-  const generateRandomId = (): void =>{
-    setRandId((Math.random()*10000)) 
-  }
-
-  useEffect(() => {
-    generateRandomId();
-    const animate = document.querySelector(`[data-animate="${randId}"]`) as HTMLElement | null;
-    animate?.style.setProperty("--Height", `${boxHeight ? boxHeight : "200"}px`);
-    console.log(animate);
-    const scrollHandler = () => {
-      if (isElementInViewport(animate)) setIsInViewPort(true);
-    };
-    window.addEventListener("scroll", scrollHandler);
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, [boxHeight, isInViewPort]);
+  
+  const {randId, isInViewPort} = useAnimation(boxHeight)
 
   return (
     <div
