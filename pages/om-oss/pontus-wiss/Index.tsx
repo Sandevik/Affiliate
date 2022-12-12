31c37db7d87987b402c42028420f2a2841ca1839
[Simon] <[simon.sandevik@outlook.com]>
2022-12-12 14:51:14 +0100

    om-oss

---
+++ pages/om-oss/pontus-wiss/Index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import StandardContentBlock from '../../../components/Blocks/StandardContentBlock'
+import SubHero from '../../../components/SubHero'
+import { useTheme } from '../../../context/ThemeContext'
+import style from "../../styles/Page.module.css"
+
+export default function Index() {
+  const {darkMode} = useTheme()
+  return (
+    <div className={darkMode ? `${style.container} ${style.darkmode}` : style.container}>
+      <SubHero>
+        <h1>Om Pontus Wiss</h1>
+        <p>Här är allt du behöver veta </p>
+      </SubHero>
+
+      
+    </div>
+  )
+}
