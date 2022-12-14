f78688dea75a9b0a075574b362f0589aadf5079e
[Simon] <[simon.sandevik@outlook.com]>
2022-12-14 14:21:14 +0100

    Hero updare

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,12 +10,11 @@ export default function Hero() {
         <div className={style.h1}>
           <h1>Jämför omstartslån</h1>
           <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum atque distinctio libero sapiente placeat ad consequatur. Libero fuga, minus distinctio similique dolore provident ad aliquid numquam ullam, quae asperiores illum.</p>
-          <h3>Punktlista</h3>
+          <h3>Våra tjänster</h3>
           <ul className={style.list}>
-            <li>lorem ipsum</li>
-            <li>lorem ipsum</li>
-            <li>lorem ipsum</li>
-            <li>lorem ipsum</li>
+            <li>Förklarar</li>
+            <li>Hjälper dig jämföra</li>
+            <li>Kostnadsfritt</li>
           </ul>
           <button className={style.cta}><a href="https://likvidum.se" target="_blank">Jämför nu</a></button>
         </div>
